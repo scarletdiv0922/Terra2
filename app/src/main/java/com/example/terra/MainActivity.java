7ce6405373d2c67169346634a636a8df020625c4
a4awesome <abhitha.vegi@gmail.com>
2020-05-23 23:54:11 -0700

    alan ai button

--- app/src/main/java/com/example/terra/MainActivity.java
+++ app/src/main/java/com/example/terra/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         alanButton = findViewById(R.id.alanbutton);
-
         AlanConfig config = AlanConfig.builder()
                 .setProjectId("9852fc5b086a6dc66a51e5f59c5b909a2e956eca572e1d8b807a3e2338fdd0dc/stage")
                 .build();
