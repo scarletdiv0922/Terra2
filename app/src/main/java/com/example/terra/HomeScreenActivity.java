0f094e8b3e790632ed3d2d9a7dbe7a6c2196b652
Kavya Verma <kavya.verma@gmail.com>
2020-05-24 13:59:58 -0700

    added FABs and intents

--- app/src/main/java/com/example/terra/HomeScreenActivity.java
+++ app/src/main/java/com/example/terra/HomeScreenActivity.java
@@ -1,6 +1,9 @@
 package com.example.terra;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +14,27 @@ public class HomeScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
         //add intent
+
+        Button menuFAB = findViewById(R.id.menu_button);
+        Button emergencyContactsFAB = findViewById(R.id.emergency_contacts_button);
+        Button infoFAB = findViewById(R.id.info_button);
+
+        emergencyContactsFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(HomeScreenActivity.this, EmergencyContactsActivity.class);
+
+            }
+        });
+
+        infoFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(HomeScreenActivity.this, BDAActivity.class);
+
+            }
+        });
     }
 }
