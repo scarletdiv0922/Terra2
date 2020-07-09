73b11da9f765fe38ea78fe7e317055cfe4c568f7
a4awesome <abhitha.vegi@gmail.com>
2020-07-08 17:31:30 -0700

    users can sign up

--- app/src/main/java/com/example/terra/SignupActivity.java
+++ app/src/main/java/com/example/terra/SignupActivity.java
@@ -2,12 +2,22 @@ package com.example.terra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignupActivity extends AppCompatActivity {
+
+    private EditText editTextEmail;
+    private EditText editTextPass;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +25,47 @@ public class SignupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
         getSupportActionBar().hide();
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+
+        editTextEmail = findViewById(R.id.email);
+        editTextPass = findViewById(R.id.password);
+
         Button signup = findViewById(R.id.sign_up_button);
         signup.setOnClickListener((v) -> {
 //            Toast.makeText(this, "Moving to home activity", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(getBaseContext(), HomeScreenActivity.class);
-            startActivity(intent);
+            registerUser();
         });
+    }
+    private void registerUser(){
+        String email = editTextEmail.getText().toString().trim();
+        String pass = editTextPass.getText().toString().trim();
+        if (TextUtils.isEmpty(email)){
+            //user needs to type the email
+            Toast.makeText(this, "Please enter your email.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(pass)){
+            //user needs to type the password
+            Toast.makeText(this, "Please enter your password.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //email and password are entered
+
+        progressDialog.setMessage("Registering user...");
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()){
+                        //user is successfully registered
+                        Toast.makeText(this, "User is successfully registered.", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(getBaseContext(), HomeScreenActivity.class);
+                        startActivity(intent);
+                    }
+                    else
+                        Toast.makeText(this, "Failed to register. Please try again.", Toast.LENGTH_LONG).show();
+                });
     }
 }
