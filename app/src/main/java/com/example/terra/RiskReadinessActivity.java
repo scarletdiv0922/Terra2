6e7f1873e04c604eb98297d0c5acce1c5fce0f46
a4awesome <abhitha.vegi@gmail.com>
2020-07-24 17:50:37 -0700

    added risk score and the data files

--- app/src/main/java/com/example/terra/RiskReadinessActivity.java
+++ app/src/main/java/com/example/terra/RiskReadinessActivity.java
@@ -1,13 +1,22 @@
 package com.example.terra;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.alan.alansdk.AlanCallback;
 import com.alan.alansdk.AlanConfig;
@@ -19,24 +28,71 @@ import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.ml.common.FirebaseMLException;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelManager;
+import com.google.firebase.ml.custom.FirebaseCustomLocalModel;
+import com.google.firebase.ml.custom.FirebaseCustomRemoteModel;
+import com.google.firebase.ml.custom.FirebaseModelDataType;
+import com.google.firebase.ml.custom.FirebaseModelInputOutputOptions;
+import com.google.firebase.ml.custom.FirebaseModelInputs;
+import com.google.firebase.ml.custom.FirebaseModelInterpreter;
+import com.google.firebase.ml.custom.FirebaseModelInterpreterOptions;
+import com.google.firebase.ml.custom.FirebaseModelOutputs;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.FloatBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import org.tensorflow.lite.Interpreter;
 
 public class RiskReadinessActivity extends AppCompatActivity {
 
     private Firebase mRef;
-    int checkedItems;
+    int checkedItems; //stores weighted amount of how many items are checked
     public int total;
     public int SIZE_OF_CHECKLIST;
     public double readinessScore;
     ImageButton back;
     FloatingActionButton home;
-    String disaster;
+    String disaster = " ";
     private static final String TAG = "RiskReadiness";
+
+    FirebaseModelInterpreterOptions options;
+    FirebaseModelInputOutputOptions inputOutputOptions;
+    FirebaseModelInterpreter interpreter;
+    FirebaseCustomLocalModel localModel;
+    FirebaseModelInputs inputs;
+    FirebaseCustomRemoteModel remoteModel;
+    Float[][][] myInput = new Float[1][1][18]; //contains whether users have a certain item or not
+    public double riskScore;
+
+    public double riskFromLoc = 0.0;
+    String county = "Alameda";
+    Button doneButton;
+
+    HashMap chklistOrder = new HashMap<String, Integer>();
+    public int itemsMarked = 0; //just the number of items marked without weight
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -192,8 +248,10 @@ public class RiskReadinessActivity extends AppCompatActivity {
                         Intent intent = new Intent(RiskReadinessActivity.this, HomeScreenActivity.class);
                         startActivity(intent);
                     }
-                    else if (cmd.substring(i, j).equals("emergency contacts") || cmd.substring(i, j).equals("contacts"))
-                        alan_button.playText("You are already at the emergency contacts screen.");
+                    else if (cmd.substring(i, j).equals("emergency contacts") || cmd.substring(i, j).equals("contacts")){
+                        Intent intent = new Intent(RiskReadinessActivity.this, EmergencyContactsActivity.class);
+                        startActivity(intent);
+                    }
                     else if (cmd.substring(i, j).equals("emergency checklist") || cmd.substring(i, j).equals("checklist")) {
                         Intent intent = new Intent(RiskReadinessActivity.this, ChecklistActivity2.class);
                         startActivity(intent);
@@ -222,9 +280,145 @@ public class RiskReadinessActivity extends AppCompatActivity {
             disaster = (String) bundle.get("Disaster");
         }
 
+        for (int i = 0; i < 18; i++)
+            myInput[0][0][i] = Float.valueOf(0);
+
         getFirebase();
 
         Toast.makeText(this, "score " + readinessScore, Toast.LENGTH_SHORT).show();
+
+        //current order of the checklist for ml model, no dust masks for now
+        chklistOrder.put("Battery-powered Radio", 1);
+        chklistOrder.put("Extra Batteries", 2);
+        chklistOrder.put("Extra Cash", 3);
+        chklistOrder.put("First-aid Kit", 4);
+        chklistOrder.put("Flashlight", 5);
+        chklistOrder.put("Garbage Bags", 6);
+        chklistOrder.put("Hand Sanitizer", 7);
+        chklistOrder.put("Local Maps", 8);
+        chklistOrder.put("Manual Can Opener", 9);
+        chklistOrder.put("Matchsticks", 10);
+        chklistOrder.put("Moist Towelettes", 11);
+        chklistOrder.put("Non-perishable Food", 12);
+        chklistOrder.put("Portable Charger", 13);
+        chklistOrder.put("Prescription Medications", 14);
+        chklistOrder.put("Water", 15);
+        chklistOrder.put("Whistles", 16);
+        chklistOrder.put("Wrench or Pliers", 17);
+
+        //risk of natural disaster based on location
+        doneButton = findViewById(R.id.doneButton);
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editTextCounty = findViewById(R.id.editTextCounty);
+                county = editTextCounty.getText().toString();
+                System.out.println(county);
+                InputStream inputStream; //default to earthquakes
+                inputStream = getResources().openRawResource(R.raw.eqdata);;
+                if (disaster.equalsIgnoreCase("Earthquakes"))
+                    inputStream = getResources().openRawResource(R.raw.eqdata);
+                else if (disaster.equalsIgnoreCase("Wildfires"))
+                    inputStream = getResources().openRawResource(R.raw.firedata);
+                System.out.println("which disaster? it's " + disaster);
+                CSVFile csvFile = new CSVFile(inputStream);
+                ArrayList<ArrayList<String>> riskList = csvFile.read();
+                System.out.println(riskList.get(1));
+                System.out.println("COUNTYYYYYYYY");
+                for(ArrayList<String> scoreData:riskList ) {
+                    for (String s:scoreData) {
+                        if (s.equals(county)) {
+                            String locRisk = scoreData.get(2);
+                            if (locRisk.equals("High"))
+                                riskFromLoc = 2.0;
+                            else if (locRisk.equals("Medium"))
+                                riskFromLoc = 1.0;
+                            else
+                                riskFromLoc = 0.5;
+                            System.out.println("At " + locRisk + " risk");
+                        }
+                    }
+                }
+                //risk score mathematical model
+                double ratio = 0.5;
+                if (itemsMarked != 0 && checkedItems != 0)
+                    ratio = (double)((checkedItems - itemsMarked)/checkedItems) + 0.1;
+                riskScore = ratio * riskFromLoc;
+                System.out.println("risk score: " + riskScore);
+
+            }
+        });
+
+        //Risk Score ml model
+//        remoteModel = new FirebaseCustomRemoteModel.Builder("EQ_model").build();
+//        localModel = new FirebaseCustomLocalModel.Builder()
+//                .setAssetFilePath("eq_model.tflite")
+//                .build(); //local and remote model in case of no internet
+//        FirebaseModelDownloadConditions conditions = new FirebaseModelDownloadConditions.Builder()
+//                .requireWifi()
+//                .build();
+//        FirebaseModelManager.getInstance().download(remoteModel, conditions)
+//                .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                    @Override
+//                    public void onSuccess(Void v) {
+//                        Toast.makeText(RiskReadinessActivity.this, "Model downloaded!", Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//        FirebaseModelManager.getInstance().isModelDownloaded(remoteModel)
+//                .addOnSuccessListener(new OnSuccessListener<Boolean>() {
+//                    @Override
+//                    public void onSuccess(Boolean isDownloaded) {
+//                        //use either remote model in firebase or local model
+//                        if (isDownloaded) {
+//                            System.out.println("remote");
+//                            options = new FirebaseModelInterpreterOptions.Builder(remoteModel).build();
+//                        }
+//                        else {
+//                            System.out.println("local");
+//                            options = new FirebaseModelInterpreterOptions.Builder(localModel).build();
+//                        }
+//
+//                        try {
+//                            System.out.println(87);
+//                            FirebaseModelInterpreter interpreter = FirebaseModelInterpreter.getInstance(options);
+//                            inputOutputOptions = new FirebaseModelInputOutputOptions.Builder()
+//                                    .setInputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, 1, 18})
+//                                    .setOutputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, 1})
+//                                    .build();
+//                            //get myInput in the getFirebase method
+//                            inputs = new FirebaseModelInputs.Builder()
+//                                    .add(myInput)  // add() as many input arrays as your model requires
+//                                    .build();
+//                            System.out.println(107);
+//                            interpreter.run(inputs, inputOutputOptions)
+//                                    .addOnSuccessListener(
+//                                            new OnSuccessListener<FirebaseModelOutputs>() {
+//                                                @Override
+//                                                public void onSuccess(FirebaseModelOutputs result) {
+//                                                    Toast.makeText(RiskReadinessActivity.this, "Model is running!", Toast.LENGTH_SHORT).show();
+//                                                    float[][] output = result.getOutput(0);
+//                                                    System.out.println("probability: " + output[0][0]);
+//                                                    riskScore = readinessScore * output[0][0];
+//                                                }
+//                                            })
+//                                    .addOnFailureListener(
+//                                            new OnFailureListener() {
+//                                                @Override
+//                                                public void onFailure(@NonNull Exception e) {
+//                                                    Toast.makeText(RiskReadinessActivity.this, "Model failed to run.", Toast.LENGTH_SHORT).show();
+//                                                    e.printStackTrace();
+//                                                }
+//                                            });
+//                        } catch (FirebaseMLException e) {
+//                            e.printStackTrace();
+//                            //continue with mathematical model
+//                            double ratio = 1.0;
+//                            if (itemsMarked != 0 && checkedItems != 0)
+//                                ratio = (double)((checkedItems - itemsMarked)/itemsMarked);
+//
+//                        }
+//                    }
+//                });
 
         //Buttons
         back = findViewById(R.id.back_button);
@@ -241,8 +435,8 @@ public class RiskReadinessActivity extends AppCompatActivity {
 
         home.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent1 = new Intent(RiskReadinessActivity.this, DisasterMenuActivity.class);
+            public void onClick(View v) { //was pointing to disaster menu activity... changed it to this?
+                Intent intent1 = new Intent(RiskReadinessActivity.this, HomeScreenActivity.class);
                 startActivity(intent1);
             }
         });
@@ -261,27 +455,38 @@ public class RiskReadinessActivity extends AppCompatActivity {
                     for (Map.Entry<String, Object> entry : map.entrySet()) {
                         Boolean value = (Boolean) entry.getValue();
                         String item = entry.getKey();
-                            if (item.equals("Water") || item.equals("Non-perishable Food") || item.equals("First-aid Kit")) {
-                                SIZE_OF_CHECKLIST +=3;
-                                System.out.println("rough");
-                                if (value){
-                                    checkedItems += 3;
-                                }
+                        //getting which items are checked on the checklist
+                        if (chklistOrder.get(item) != null) {
+                            int idx = (int) chklistOrder.get(item);
+                            if (value) {
+                                myInput[0][0][idx] = Float.valueOf(1);
+                                itemsMarked++;
                             }
-                            else if (item.equals("Portable Charger") || item.equals("Extra Cash") || item.equals("Flashlight") || item.equals("Extra Batteries")) {
-                                SIZE_OF_CHECKLIST +=2;
-                                System.out.println("buddy");
-                                if (value){
-                                    checkedItems += 2;
-                                }
+                            else
+                                myInput[0][0][idx] = Float.valueOf(0);
+                        }
+                        //get readiness score
+                        if (item.equals("Water") || item.equals("Non-perishable Food") || item.equals("First-aid Kit")) {
+                            SIZE_OF_CHECKLIST +=3;
+                            System.out.println("rough");
+                            if (value){
+                                checkedItems += 3;
                             }
-                            else {
-                                SIZE_OF_CHECKLIST++;
-                                System.out.println("- Z");
-                                if (value){
-                                    checkedItems ++;
-                                }
+                        }
+                        else if (item.equals("Portable Charger") || item.equals("Extra Cash") || item.equals("Flashlight") || item.equals("Extra Batteries")) {
+                            SIZE_OF_CHECKLIST +=2;
+                            System.out.println("buddy");
+                            if (value){
+                                checkedItems += 2;
                             }
+                        }
+                        else {
+                            SIZE_OF_CHECKLIST++;
+                            System.out.println("- Z");
+                            if (value){
+                                checkedItems ++;
+                            }
+                        }
                     }
                     Firebase mRefChild = mRef.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("readiness_score");
                     readinessScore = (double) checkedItems / (double) SIZE_OF_CHECKLIST;
